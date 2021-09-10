9a5add1b3515a5d3b78c4b02c0a40f5ee42930ba
Simone Denardi <simone.denardi@lasportiva.com>
2021-09-10 09:24:02 +0200

    Aggiungere i file di progetto.

---
+++ pdfStampa/Form1.cs
@@ -0,0 +1,37 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pdfStampa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            var path = @"C:\Users\simone.denardi\Desktop\pdfs\000001a_50_11415_040121.pdf";
+            using (var document = PdfDocument.Load(path))
+            {
+                using (var printDocument = document.CreatePrintDocument())
+                {
+                    printDocument.PrinterSettings.PrintFileName = "000001a_50_11415_040121.pdf";
+                    printDocument.PrinterSettings.FromPage = 1;
+                    printDocument.PrinterSettings.ToPage = 1;
+                    printDocument.PrinterSettings.PrinterName = @"UfficioIT";
+                    printDocument.DocumentName = "000001a_50_11415_040121.pdf";
+                    printDocument.PrinterSettings.PrintFileName = "000001a_50_11415_040121.pdf";
+                    printDocument.PrintController = new StandardPrintController();
+                    printDocument.Print();
+                }
+            }
+        }
+    }
+}
